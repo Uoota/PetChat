51c65cd6fe75ac61d5c036239469b437fb46a926
Nikita Romanov <nromanov@rvision.ru>
2023-01-31 17:08:34 +0300

    first lasson

--- app/src/main/java/com/example/petchat/MainActivity.kt
+++ app/src/main/java/com/example/petchat/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.petchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import com.example.petchat.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.util.DrawerNavigationUI
+import com.mikepenz.materialdrawer.widget.AccountHeaderView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDriver: DrawerNavigationUI
+    private lateinit var mHeader: AccountHeaderView
+    private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+    }
+
+    private fun initFields() {
+
     }
 }
